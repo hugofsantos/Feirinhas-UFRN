fdbff10b96c4bda76c456987cc3f21327f280c91
Hugo Santos <hf.santos2332@gmail.com>
2025-06-19 21:06:09 -0300

    Commit inicial

---
+++ src/main/java/br/imd/ufrn/feirinhas_ufrn/FeirinhasUfrnApplication.java
@@ -0,0 +1,13 @@
+package br.imd.ufrn.feirinhas_ufrn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeirinhasUfrnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeirinhasUfrnApplication.class, args);
+	}
+
+}
